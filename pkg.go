8fb9d5470d131bb40aa73d5130c94d8bbcc34506
Evan Lezar <elezar@mesosphere.com>
2017-09-28 21:22:33 +0200

    Add loading of yaml file to Package

--- pkg.go
+++ pkg.go
@@ -1,13 +1,23 @@
 package pkg
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
 // DefaultPackagingVerison is the defaul packing version
 const DefaultPackagingVerison string = "4.0"
 
+const DefaultMinDcosReleaseVersion string = "1.9"
+
 // {
-// 	"packagingVersion": "4.0",
-// 	"upgradesFrom": ["{{upgrades-from}}"],
-// 	"downgradesTo": ["{{downgrades-to}}"],
-// 	"minDcosReleaseVersion": "1.9",
+// "packagingVersion": "4.0",
+// "upgradesFrom": ["{{upgrades-from}}"],
+// "downgradesTo": ["{{downgrades-to}}"],
+// "minDcosReleaseVersion": "1.9",
 // 	"name": "beta-cassandra",
 // 	"version": "{{package-version}}",
 // 	"maintainer": "support@mesosphere.io",
@@ -20,13 +30,74 @@ const DefaultPackagingVerison string = "4.0"
 // 	"postUninstallNotes": "The DC/OS Apache Cassandra service is being uninstalled.\n\nFor DC/OS versions from 1.10 no further action is required. For older DC/OS versions follow the instructions at {{documentation-path}}uninstall to remove any persistent state if required."
 //   }
 type Package struct {
-	packagingVersion string `yaml:"packagingVersion"`
+	Name                  string   `json:"name"`
+	PackagingVersion      string   `json:"packagingVersion"`
+	MinDcosReleaseVersion string   `json:"minDcosReleaseVersion"`
+	UpgradesFrom          []string `json:"upgradesFrom"`
+	DowngradesTo          []string `json:"DowngradesTo"`
+	Selected              bool
+	Framework             bool `json:default=true`
+	Description           string
 }
 
 // NewPackage constructs a new package with default values
 func NewPackage() Package {
 	p := Package{}
-	p.packagingVersion = DefaultPackagingVerison
+	p.PackagingVersion = DefaultPackagingVerison
 
 	return p
+}
+
+func (c *Package) ParseYaml(data []byte) error {
+	if err := yaml.Unmarshal(data, c); err != nil {
+		return err
+	}
+	c.setDefaults()
+	return nil
+}
+
+func (c *Package) setDefaults() {
+	if c.PackagingVersion == "" {
+		c.PackagingVersion = DefaultPackagingVerison
+	}
+	if c.MinDcosReleaseVersion == "" {
+		c.MinDcosReleaseVersion = DefaultMinDcosReleaseVersion
+	}
+	if c.UpgradesFrom == nil {
+		c.UpgradesFrom = []string{"{{upgrades-from}}"}
+	}
+	if c.DowngradesTo == nil {
+		c.DowngradesTo = []string{"{{downgrades-to}}"}
+	}
+}
+
+func LoadYamlFile(filename string) (Package, error) {
+	p := Package{}
+
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return p, err
+	}
+	err = p.ParseYaml(raw)
+	if err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+
+func LoadJsonFile(filename string) (Package, error) {
+	p := Package{}
+	file, err := os.Open(filename)
+	if err != nil {
+		return p, err
+	}
+	dec := json.NewDecoder(file)
+
+	err = dec.Decode(&p)
+	if err != nil {
+		return p, err
+	}
+
+	return p, err
 }
--- pkg_test.go
+++ pkg_test.go
@@ -1,13 +1,59 @@
 package pkg
 
 import (
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDetaultPackageInfo(t *testing.T) {
+	p := Package{}
+	err := p.ParseYaml([]byte{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, "", p.Name)
+	assert.Equal(t, DefaultPackagingVerison, p.PackagingVersion)
+	assert.Equal(t, false, p.Selected)
+	assert.Equal(t, true, p.Framework)
+	assert.Equal(t, DefaultMinDcosReleaseVersion, p.MinDcosReleaseVersion)
+	assert.Equal(t, "", p.Description)
+	assert.Equal(t, []string{"{{upgrades-from}}"}, p.UpgradesFrom)
+	assert.Equal(t, []string{"{{downgrades-to}}"}, p.DowngradesTo)
+}
+
 func TestDefaultPackageVersionIsCorrect(t *testing.T) {
 	p := NewPackage()
 
-	assert.Equal(t, "4.0", p.packagingVersion)
+	assert.Equal(t, "4.0", p.PackagingVersion)
+}
+
+func TestDefaultIsSetFromYaml(t *testing.T) {
+	packageText := `
+packagingVersion: 4.0
+name: foo
+bar: baz
+`
+	var p Package
+
+	fmt.Print(packageText)
+	err := p.ParseYaml([]byte(packageText))
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, "foo", p.Name)
+
+}
+
+func TestLoadYamlFromFile(t *testing.T) {
+	p, err := LoadYamlFile("examples/package-only.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, "package-name", p.Name)
+	assert.Equal(t, DefaultPackagingVerison, p.PackagingVersion)
 }
